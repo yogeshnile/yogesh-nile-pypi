8a1819f97a88b2377885c391bf1691953711470f
yogeshnile <yogeshnile786@gmail.com>
2020-11-15 17:57:16 +0530

    Yogesh Nile Library ready

---
+++ Yogesh_Nile/__init__.py
@@ -0,0 +1,154 @@
+class resume():
+    def get_Objective(self):
+        '''
+        This function contain Yogesh Nile's Objective.
+
+        Example:-\n
+        Objective = obj.get_Objective()\n
+        print(Objective)
+        '''
+        return '''\nHaving an entry level of experience seeking a challenging position in the field of DevOps, Python Developer so as to utilize my skill for organizarion and individual growth.\n'''
+
+    def get_Info(self):
+        '''
+        This function contain Yogesh Nile's Basic Info.
+
+        Example:-\n
+        Info = obj.get_Info()\n
+        print(Info)
+        '''
+        return '''
+#############################################
+# Name:-        Yogesh Nile                 #
+# Email Id:-    yogeshnile.work4u@gmail.com #
+# Github:-      yogeshnile                  #
+# Linkedin:-    yogesh-nile                 #
+# Twitter:-     YogeshNile                  #
+#############################################
+        '''
+
+    def get_Introduction(self):
+        '''
+        This function contain Yogesh Nile's Introduction.
+
+        Example:-\n
+        Introduction = obj.get_Introduction()\n
+        print(Introduction)
+        '''
+        return "My name is Yogesh Nile. I'm a 3rd-year BCA(Bachelor of Computer Application) student from Vimal Tormal Poddar BCA Collage (Affliated to Veer Narmad South Gujarat University), Surat. I hail from a small village of Maharashtra, India. I started my coding journey in 2017 when I want to did something in computer field. In which devoting 3 years to BCA, in 2018 I shifted my interest into the field of software engineering. Since then, I breathe in code."
+
+    def get_Skills(self):
+        '''
+        This function contain Yogesh Nile's Skills.
+
+        Example:-\n
+        Skills = obj.get_Skills()\n
+        print(Skills)
+        '''
+        return '''
+1. Sound knowledge of Python, UNIX, PHP, SQL, HTML, CSS, JavaScript languages.
+2. Sound knowledge of Cloud Computing (GCP, AWS).
+3. Strong knowledge of Data Analytics.
+4. Do everything in python. (Django, Machine Learning, GUI App, Web Scraping, Bots, Game, etc)
+5. Strong verbal and communication skills.
+6. Problem analysis and solving skills.
+7. Development Environment: Ubuntu 18.04 (Using), Jupyter Notebook, Spyder, Visual Studio Code as a text editor, Sublime Text, Nano.
+8. Version Control: One and only GitHub
+        '''
+
+    def get_Academia(self):
+        '''
+        This function contain Yogesh Nile's Academia.
+
+        Example:-\n
+        academia = obj.get_Academia()\n
+        print(academia)
+        '''
+        return '''
+========================================================================
+| Sno. | Degree  | Board or University  | Year of Passing | Percentage |
+========================================================================
+|  1   |  B.C.A. |     V.N.S.G.U.       |     Pursuing    |     ---    |
+------------------------------------------------------------------------
+|  2   |   XII   |       G.S.E.B.       |       2015      |   65.00 %  |
+------------------------------------------------------------------------
+|  3   |    X    |       G.S.E.B.       |       2013      |   70.35 %  |
+========================================================================
+        '''
+    def get_Links(self):
+        '''
+        This function contain Yogesh Nile's Social Links.
+
+        Example:-\n
+        Links = obj.get_Links()\n
+        print(Links)
+        '''
+        return '''
+-------------------------------------------------
+# 1. Linkedin   :-  https://bit.ly/2Ky3ho6      #
+# 2. Twitter    :-  https://bit.ly/3dbLJLC      #
+# 3. Github     :-  https://bit.ly/2yoggit      #
+# 4. Discord    :-  https://discord.gg/R2ug3gR  #
+# 5. Instagram  :-  https://bit.ly/32SXHV0      #
+# 6. Resume     :-  https://yogeshnile.cf       #
+-------------------------------------------------
+        '''
+    def get_Certification_Course(self):
+        '''
+        This function contain Yogesh Nile's Certification_Course.
+
+        Example:-\n
+        Course = obj.get_Certification_Course()\n
+        print(Course)
+        '''
+        return '''
+=====================================
+# 1. DevOps                         #
+# 2. Cloud Computing                #
+# 3. Machine Learning for Beginners #
+# 4. Data Analysis                  #
+=====================================
+        '''
+    def get_Portfolio(self):
+        '''
+        This function contain Yogesh Nile's Coding Portfolio.
+
+        Example:-\n
+        Portfolio = obj.get_Portfolio()\n
+        print(Portfolio)
+        '''
+        return '''
+##################################################################################################
+    ## Machine Learning ##
+1. Restaurant Review Sentiment Analysis     :-  https://github.com/yogeshnile/Restaurant-Review-Sentiment-Analysis
+2. IPL First Inning Score Predict           :-  https://github.com/yogeshnile/IPL-First-Inning-Score-Predict
+3. Spam SMS Detector                        :-  https://github.com/yogeshnile/spam-sms-detector
+4. Twitter Sentiment Analysis on FlaskApp   :-  https://github.com/yogeshnile/Twitter-Sentiment-Analysis-on-Flask-App
+5. Movies Genre Classification              :-  https://github.com/yogeshnile/Movies-Genre-Classification
+6. Nifty50 Index Time Series Analysis       :-  https://github.com/yogeshnile/Nifty50-Index-Time-Series-Analysis
+    ## Bots ##
+7. Twitter Bot                              :-  https://github.com/yogeshnile/twitter-bot
+    ## Django ##
+8. Student Result Management System         :-  https://github.com/yogeshnile/SRMS
+9. Course Stream web Application            :-  https://github.com/yogeshnile/course-stream-in-django
+    ## Python Script ##
+10. Fetch old tweets                        :-  https://github.com/yogeshnile/fetch-old-tweet
+11. Tic Tac Toe Game on Python GUI          :-  https://github.com/yogeshnile/Tic-Tac-Toe-Game-on-Python-GUI
+12. Snake game in Python                    :-  https://github.com/yogeshnile/Snake-game-in-python
+    ## Unix ##
+13. Shell Script                            :-  https://github.com/yogeshnile/unix
+
+##################################################################################################
+        '''
+    def get_LinkedIn_Recommendations(self):
+        '''
+        This function contain Yogesh Nile's LinkedIn_Recommendations Link.
+
+        Example:-\n
+        recommendation = obj.get_LinkedIn_Recommendations()\n
+        print(recommendation)
+        '''
+        return '''
+Check out below link
+https://yogeshnile.cf/static/linkedin%20recommendation.png 
+'''
